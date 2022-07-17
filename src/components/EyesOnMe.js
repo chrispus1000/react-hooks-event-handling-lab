dd4b84484cccf898a9d07dead6465ec1027ac012
chrispus1000 <minrevac@gmail.com>
2022-07-17 15:10:02 +0300

    submit assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe() {
+
+    function handleFocus() {
+        console.log('Good!')
+    }
+
+    function handleBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button type="password" onFocus={handleFocus} onBlur={handleBlur}>
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe;
